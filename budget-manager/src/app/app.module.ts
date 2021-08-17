5db826a1a7a2465f05899d63b27ca579852f46ad
Catalin <catalin.bajenaru@uipath.com>
2021-08-17 15:39:58 +0300

    chore: remove MatModule and minor fixes

--- budget-manager/src/app/app.module.ts
+++ budget-manager/src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {
+  MatNativeDateModule,
+  MatOptionModule,
+} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+  ManageBudgetDialog,
+} from 'src/modals/manage-budget/manage-budget-dialog';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AllMaterialModule } from './material.module';
 import { BudgetComponent } from './budget/budget.component';
 import { SpendingsModule } from './spendings/spendings.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ManageBudgetDialog } from 'src/modals/manage-budget/manage-budget-dialog';
 
 @NgModule({
   declarations: [
@@ -21,11 +42,25 @@ import { ManageBudgetDialog } from 'src/modals/manage-budget/manage-budget-dialo
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AllMaterialModule,
     HttpClientModule,
     SpendingsModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    MatOptionModule,
+    MatListModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
