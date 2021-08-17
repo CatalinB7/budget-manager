9914c626a00ea93d4decf5aed8c18581cf47ea9e
sebastian <sebastian.dochia@uipath.com>
2021-08-17 11:10:52 +0300

    feat(spending-service): add error handling and operations before request

--- budget-manager/src/app/services/spending.service.ts
+++ budget-manager/src/app/services/spending.service.ts
@@ -35,6 +35,14 @@ export class SpendingService {
     this.spendingList$.subscribe(data => this.spendingList = data);
   }
 
+  handleResponse(data: string) {
+    if (data === "OK") {
+      this.computeTotals();
+    } else {
+      location.reload();
+    }
+  }
+
   computeTotals() {
     this.spendingTotals$.next(this.spendingList.map((category) => {
       let total = 0;
@@ -47,16 +55,21 @@ export class SpendingService {
     }));
   }
 
+  getCategotyPosition(categoryName: string): number {
+    return this.spendingList.map((obj) => obj.name).indexOf(categoryName);
+  }
+
+  getSpendingPosition(categoryPosition: number, spendingId: string): number {
+    return this.spendingList[categoryPosition].expenses.map(obj => obj.id).indexOf(spendingId);
+  }
+
   getSpendingList(): Observable<ISpendingCategory[]> {
     return this._http.get('http://localhost:3000/expenses_categories?userId=1').pipe(
       map((data: any) => data[0].categories),
       tap(data => {
-        console.log(data);
         this.spendingList$.next(data);
 
         this.computeTotals();
-
-        console.log(this.spendingTotals$);
       })
     );
   }
@@ -75,40 +88,67 @@ export class SpendingService {
       spending: newSpending
     };
 
-    this.spendingList.filter(obj => obj.name == spending.category)[0].expenses.push(newSpending);
+    this.spendingList[this.getCategotyPosition(spending.category)].expenses.push(newSpending);
     this.spendingList$.next(this.spendingList);
 
     return this._http.post('http://localhost:3000/expenses_categories/spendings?userId=1', body, { responseType: "text" }).pipe(
       tap(data => {
-        console.log(data);
-        if (data === "OK") {
-          this.computeTotals();
-        } else {
-          location.reload();
-        }
+        this.handleResponse(data);
       })
     );
   }
 
   addSpendingCategory(categoryName: string) {
-    let body: ISpendingCategory = { name: categoryName, expenses: [] };
-    return this._http.post('http://localhost:3000/expenses_categories/categories?userId=1', body, { responseType: "text" });
+    const body: ISpendingCategory = { name: categoryName, expenses: [] };
+
+    this.spendingList.push(body);
+    this.spendingList$.next(this.spendingList);
+
+    return this._http.post('http://localhost:3000/expenses_categories/categories?userId=1', body, { responseType: "text" }).pipe(
+      tap(data => {
+        this.handleResponse(data);
+      })
+    );
   }
 
   removeSpending(categoryName: string, spendingId: string) {
-    return this._http.delete(`http://localhost:3000/expenses_categories/spendings?userId=1&spendingId=${spendingId}&category=${categoryName}`, { responseType: "text" });
+    const categoryPosition = this.getCategotyPosition(categoryName);
+    const spendingPosition = this.getSpendingPosition(categoryPosition, spendingId);
+
+    this.spendingList[categoryPosition].expenses.splice(spendingPosition, 1);
+    this.spendingList$.next(this.spendingList);
+
+    return this._http.delete(`http://localhost:3000/expenses_categories/spendings?userId=1&spendingId=${spendingId}&category=${categoryName}`, { responseType: "text" }).pipe(
+      tap(data => {
+        this.handleResponse(data);
+      })
+    );
   }
 
   removeSpendingCategory(categoryName: string) {
-    return this._http.delete(`http://localhost:3000/expenses_categories/categories?userId=1&name=${categoryName}`, { responseType: "text" });
+    const categoryPosition = this.getCategotyPosition(categoryName);
+
+    this.spendingList.splice(categoryPosition, 1);
+    this.spendingList$.next(this.spendingList);
+
+    return this._http.delete(`http://localhost:3000/expenses_categories/categories?userId=1&name=${categoryName}`, { responseType: "text" }).pipe(
+      tap(data => {
+        this.handleResponse(data);
+      })
+    );
   }
 
   editCategory(oldCategory: string, newCategory: string) {
-    console.log("editing name", oldCategory, newCategory);
-    return this._http.put(`http://localhost:3000/expenses_categories/categories?userId=1&oldName=${oldCategory}&newName=${newCategory}`, {},
-      { responseType: "text" }).pipe(tap(data => console.log(data)));
+    const categoryPosition = this.getCategotyPosition(oldCategory);
+
+    this.spendingList[categoryPosition].name = newCategory;
+    this.spendingList$.next(this.spendingList);
+
+    return this._http.put(`http://localhost:3000/expenses_categories/categories?userId=1&oldName=${oldCategory}&newName=${newCategory}`, {}, { responseType: "text" }).pipe(
+      tap(data => {
+        this.handleResponse(data);
+      })
+    );
 
   }
-
-
 }
