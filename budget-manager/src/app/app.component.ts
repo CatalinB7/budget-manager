7923fa601fe33992309e9bef7169d16b64799f23
sebastian <sebastian.dochia@uipath.com>
2021-08-19 14:27:12 +0300

    feat(app): use the new state system

--- budget-manager/src/app/app.component.ts
+++ budget-manager/src/app/app.component.ts
@@ -5,11 +5,8 @@ import {
 } from '@angular/core';
 
 import { IBudget } from './model/budget';
-import { ISpending } from './model/spending';
-import {
-  IComputedSpendCateg,
-  ISpendingCategory,
-} from './model/spendingCategory';
+import { ISpendingCategory } from './model/spendingCategory';
+import { ISpendingTotal } from './model/spendingTotal';
 import { BudgetService } from './services/budget.service';
 import { SpendingService } from './services/spending.service';
 
@@ -21,32 +18,25 @@ import { SpendingService } from './services/spending.service';
 })
 export class AppComponent implements OnInit {
   title = 'budget-manager';
-
   spendingList: ISpendingCategory[] = [];
-  categoryList: IComputedSpendCateg[] = []; //mapped spendingList so it contains computed total
+  spendingTotals: ISpendingTotal[] = [];
   budget: IBudget = {value: 0, plannedSaving: 0};
   colsNo = 2;
-  constructor(private _spengingService: SpendingService, private _budgetService: BudgetService) {}
 
-  ngOnInit(): void {
-    this._spengingService.getSpendingList().subscribe(list => {
-      this.spendingList = list;
-      this.categoryList = list.map(li => ({
-        name: li.name,
-        expenses: li.expenses,
-        total: li.expenses.reduce((a: number, b: ISpending) => a + b.value, 0)
-      }));
-    });
-    this._budgetService.getBudget().subscribe(budget => this.budget = budget);
+  constructor(private _spengingService: SpendingService, private _budgetService: BudgetService) {
+    this._spengingService.spendingList$.subscribe(value => this.spendingList = value);
+    this._spengingService.spendingTotals$.subscribe(value => this.spendingTotals = value);
+    this._budgetService.budget$.subscribe(value => this.budget = value);
   }
 
-
+  ngOnInit(): void {
+    this._spengingService.getSpendingList().subscribe();
+    this._budgetService.getBudget().subscribe();
+  }
 
   onResize(event: any) {
     if (event.target.innerWidth <= 1200) 
       this.colsNo = 1;
     else this.colsNo = 2;
   }
-
-
 }
