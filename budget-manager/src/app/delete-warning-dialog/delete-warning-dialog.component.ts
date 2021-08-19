c5152f5e7dfc58ccad798a07921cbbab6684fe7d
Catalin <catalin.bajenaru@uipath.com>
2021-08-19 10:08:45 +0300

    feat(delete operation): warning before deletion

---
+++ budget-manager/src/app/delete-warning-dialog/delete-warning-dialog.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  Inject,
+} from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-warning-dialog',
+  templateUrl: './delete-warning-dialog.component.html',
+  styleUrls: ['./delete-warning-dialog.component.scss']
+})
+export class DeleteWarningDialogComponent {
+
+  constructor(public dialogRef: MatDialogRef<DeleteWarningDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public toDelete: string) { }
+
+  cancelClick() {
+    this.dialogRef.close(false);
+  }
+
+  continueClick() {
+    this.dialogRef.close(true);
+  }
+}
--- budget-manager/src/app/spendings/spendings.module.ts
+++ budget-manager/src/app/spendings/spendings.module.ts
@@ -37,6 +37,7 @@ import { ShowIfTruncatedDirective } from '../show-if-truncated.directive';
 import {
   SpendingCardComponent,
 } from '../spending-card/spending-card.component';
+import { DeleteWarningDialogComponent } from '../delete-warning-dialog/delete-warning-dialog.component';
 
 @NgModule({
   declarations: [
